1cce025bd358b03d164fce5cc1b6c5bd480148a8
TyrWinDev <Tyrwinterhd@gmail.com>
2021-11-01 22:52:25 -0400

    Added some styling and functionality

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-
+import { Grid, CircularProgress } from '@material-ui/core';
 import Post from './Post/Post.js';
 import useStyles from './styles.js';
 
@@ -10,11 +10,24 @@ const Posts = () => {
 
   console.log(posts);
   return (
-    <>
-      <h1>POSTS</h1>;
-      <Post />
-      <Post />
-    </>
+    // if there are no post, then show circular progress. If there are post, then show the grid.
+    !posts.length ? (
+      <CircularProgress />
+    ) : (
+      <Grid
+        className={classes.container}
+        container
+        alignItems='stretch'
+        spacing={3}
+      >
+        {/* Map through all the post and return them as grid items*/}
+        {posts.map((post) => (
+          <Grid key={post._id} item xs={12} sm={6}>
+            <Post post={post} />
+          </Grid>
+        ))}
+      </Grid>
+    )
   );
 };
 
